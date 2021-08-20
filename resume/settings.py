b365ea0379f3cb7b0c985b829dc4e7fe5c1d365e
stephen osemeke <osemekestephen1995@outlook.com>
2021-08-20 13:25:56 +0200

    modifications

--- resume/settings.py
+++ resume/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f_(r1-czq!6g4)9w9vm_9jt#%xw=^)+wle=%54)(nlsld-@yzb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['steve-resume.herokuapp.com','localhost', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
